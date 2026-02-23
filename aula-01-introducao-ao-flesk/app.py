31eceb670a271b7d181d81d40d933ad6792d792b
ANDRÉ DE MOURA SILVA <andre.silva@e239.local>
2026-02-23 11:09:12 -0300

    Iniciando o site em Flask

---
+++ aula-01-introducao-ao-flesk/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+# Carregando o Flask em uma variável
+app = Flask(__name__, template_folder='views')  
+#__name__ é uma variável de ambiente do Python que tem o nome do módulo atual.
+
+# Criando a rota principal do site
+@app.route('/')
+# def serve para criar funções no Python
+def home():
+    return render_template('index.html')
+
+@app.route('/games')
+def games():
+    return render_template('games.html')
+
+@app.route('/Consoles')
+def consoles():
+    return render_template('consoles.html')
+
+# Iniciando o servidor web
+if __name__ == '__main__':
+    app.run(debug=True)
+    # Verificando se o app.py for o aruivo principal ele inicia o sevidor 
